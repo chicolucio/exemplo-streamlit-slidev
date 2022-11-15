1f69d3c54d936191123227b7ab885515330715c3
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2022-11-15 18:44:21 -0300

    First version

---
+++ global-bottom.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="absolute bottom-0 left-0 right-0 p-2 text-slate">Francisco Bustamante
+  <div class="abs-br mr-2 flex gap-0">
+    <a href="https://github.com/chicolucio" target="_blank" alt="GitHub"
+       class="text-xl icon-btn opacity-50 !border-none !hover:text-white">
+      <carbon-logo-github />
+    </a>
+  </div>
+    <div class="abs-br mr-10 flex gap-0">
+      <a href="https://www.linkedin.com/in/flsbustamante/" target="_blank" alt="LinkedIn"
+         class="text-xl icon-btn opacity-50 !border-none !hover:text-white">
+        <carbon-logo-linkedin />
+      </a>
+    </div>
+  </footer>
+</template>
